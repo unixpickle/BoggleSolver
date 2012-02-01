d92c25337b2e5ebfc311b179835926010f98a15a
Alex Nichol <alexnichol@comcast.net>
2012-02-01 16:21:26 -0500

    Initial Commit

---
+++ BoggleSolver/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BoggleSolver
+//
+//  Created by Alex Nichol on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
